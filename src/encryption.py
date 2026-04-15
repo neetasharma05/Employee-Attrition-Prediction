631c246190d1cc88042c12d498e81ecca64d369c
neetasharma05 <neeta.sharma@mitwpu.edu.in>
2026-04-16 00:57:58 +0530

    Update encryption.py

--- src/encryption.py
+++ src/encryption.py
@@ -1,24 +1,20 @@
 from cryptography.fernet import Fernet
 
-# Generate key
-def generate_key():
+# Generate key and save it
+def generate_key(path="leaderboard/secret.key"):
     key = Fernet.generate_key()
-    return key
-
-# Save key to file
-def save_key(key, path="secret.key"):
     with open(path, "wb") as f:
         f.write(key)
+    return key
 
-# Load key from file
-def load_key(path="secret.key"):
+# Load key
+def load_key(path="leaderboard/secret.key"):
     with open(path, "rb") as f:
         return f.read()
 
 # Encrypt file
 def encrypt_file(input_path, output_path, key):
     cipher = Fernet(key)
-
     with open(input_path, "rb") as f:
         data = f.read()
 
@@ -30,11 +26,10 @@ def encrypt_file(input_path, output_path, key):
 # Decrypt file
 def decrypt_file(input_path, output_path, key):
     cipher = Fernet(key)
-
     with open(input_path, "rb") as f:
-        encrypted_data = f.read()
+        data = f.read()
 
-    decrypted_data = cipher.decrypt(encrypted_data)
+    decrypted_data = cipher.decrypt(data)
 
     with open(output_path, "wb") as f:
         f.write(decrypted_data)
